909bb763488a7eb007ae57183724183a432717bb
Dustin Kroeger <dustin.kroeger@telekom.de>
2023-11-25 19:53:30 +0100

    update attendee name

--- src/pages/attendees.flv.tsx
+++ src/pages/attendees.flv.tsx
@@ -25,7 +25,7 @@ const AttendeesPage = () => {
                 <div key={attendee.id} className='flex m-2 border-2 border-gray-500'>
                     <img className='w-24 h-32 border-2 border-cheese2' src={attendee.expand.participant.avatar ? `${getConfig().publicRuntimeConfig.pocketbase}/api/files/users/${attendee.expand.participant.id}/${attendee.expand.participant.avatar}` : `/static/images/dog_dance_2.gif`} alt="avatar" />
                     <div>
-                        <h2 className='text-3xl ml-10'>{attendee.expand.participant.username}</h2>
+                        <h2 className='text-3xl ml-10'>{attendee.expand.participant.name ? attendee.expand.participant.name : attendee.expand.participant.username}</h2>
                         <div className='ml-10'>
                             {attendee.expand.participant.species && <div>Species: {attendee.expand.participant.species}</div>}
                             {attendee.expand.participant.gender && <div>Gender: {attendee.expand.participant.gender}</div>}
